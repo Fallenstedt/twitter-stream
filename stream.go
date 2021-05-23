bd92bbaf05d95fa569693ffea4d866dffca7a53c
Alex Fallenstedt <alex.fallenstedt@streem.com>
2021-05-23 13:17:01 -0700

    comments

--- stream.go
+++ stream.go
@@ -7,7 +7,7 @@ import (
 
 type (
 	// UnmarshalHook is a function that will unmarshal json.
-	UnmarshalHook func([]byte) interface{}
+	UnmarshalHook func([]byte) (interface{}, error)
 
 	// IStream is the interface that the stream struct implements.
 	IStream interface {
@@ -23,7 +23,11 @@ type (
 		Err  error
 	}
 
-	stream struct {
+	// Stream is the struct that manages a long running TCP connection with Twitter.
+	// It accepts an 'unamarshalHook' which allows you to unmarshal json in a thread-safe manner.
+	// It is highly encouraged to set a unmarshal hook before starting a stream. Unmarshaling json
+	// in a separate goroutine is not recommended because the Go bytes.Buffer is not thread safe.
+	Stream struct {
 		unmarshalHook UnmarshalHook
 		messages   chan StreamMessage
 		httpClient IHttpClient
@@ -32,10 +36,10 @@ type (
 	}
 )
 
-func newStream(httpClient IHttpClient, reader IStreamResponseBodyReader) *stream {
-	return &stream{
-		unmarshalHook: func(bytes []byte) interface{} {
-			return bytes
+func newStream(httpClient IHttpClient, reader IStreamResponseBodyReader) *Stream {
+	return &Stream{
+		unmarshalHook: func(bytes []byte) (interface{}, error) {
+			return bytes, nil
 		},
 		messages:   make(chan StreamMessage),
 		done:       make(chan struct{}),
@@ -44,17 +48,20 @@ func newStream(httpClient IHttpClient, reader IStreamResponseBodyReader) *stream
 	}
 }
 
-func (s *stream) SetUnmarshalHook(hook UnmarshalHook) {
+// SetUnmarshalHook sets the function that unmarshals json. It is highly encouraged
+// that you unmarshal json with this hook to promote thread-safety. Go's bytes.Buffer is not
+// thread safe and can result in panics when a bytes.Buffer is shared across goroutines.
+func (s *Stream) SetUnmarshalHook(hook UnmarshalHook) {
 	s.unmarshalHook = hook
 }
 
 // GetMessages returns the read-only messages channel
-func (s *stream) GetMessages() <-chan StreamMessage {
+func (s *Stream) GetMessages() <-chan StreamMessage {
 	return s.messages
 }
 
 // StopStream sends a close signal to stop the stream of tweets.
-func (s *stream) StopStream() {
+func (s *Stream) StopStream() {
 	close(s.done)
 }
 
@@ -62,7 +69,7 @@ func (s *stream) StopStream() {
 // Accepts query params described in GET /2/tweets/search/stream to expand the payload that is returned. Query params string must begin with a ?.
 // See available query params here https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/api-reference/get-tweets-search-stream.
 // See an example here: https://developer.twitter.com/en/docs/twitter-api/expansions.
-func (s *stream) StartStream(optionalQueryParams string) error {
+func (s *Stream) StartStream(optionalQueryParams string) error {
 	res, err := s.httpClient.getSearchStream(optionalQueryParams)
 
 	if err != nil {
@@ -76,7 +83,7 @@ func (s *stream) StartStream(optionalQueryParams string) error {
 	return nil
 }
 
-func (s *stream) streamMessages(res *http.Response) {
+func (s *Stream) streamMessages(res *http.Response) {
 	defer res.Body.Close()
 
 	for !stopped(s.done) {
@@ -94,11 +101,11 @@ func (s *stream) streamMessages(res *http.Response) {
 			continue
 		}
 
-		data := s.unmarshalHook(b)
+		data, err := s.unmarshalHook(b)
 
 		s.messages <- StreamMessage{
 			Data: data,
-			Err:  nil,
+			Err:  err,
 		}
 	}
 }
