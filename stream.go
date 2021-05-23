f7ecb71c7d9dd957552d57bb51c4057865c84f14
Alex Fallenstedt <alex.fallenstedt@streem.com>
2021-05-23 12:55:29 -0700

    unmarshal hook

--- stream.go
+++ stream.go
@@ -2,41 +2,50 @@ package twitterstream
 
 import (
 	"net/http"
-	"sync"
 )
 
+
 type (
+	// UnmarshalHook is a function that will unmarshal json.
+	UnmarshalHook func([]byte) interface{}
+
 	// IStream is the interface that the stream struct implements.
 	IStream interface {
 		StartStream(queryParams string) error
 		StopStream()
 		GetMessages() <-chan StreamMessage
+		SetUnmarshalHook(hook UnmarshalHook)
 	}
 
 	// StreamMessage is the message that is sent from the messages channel.
 	StreamMessage struct {
-		Data []byte
+		Data interface{}
 		Err  error
 	}
 
-
 	stream struct {
+		unmarshalHook UnmarshalHook
 		messages   chan StreamMessage
 		httpClient IHttpClient
 		done       chan struct{}
-		group      *sync.WaitGroup
 		reader     IStreamResponseBodyReader
 	}
 )
 
 func newStream(httpClient IHttpClient, reader IStreamResponseBodyReader) *stream {
 	return &stream{
+		unmarshalHook: func(bytes []byte) interface{} {
+			return bytes
+		},
 		messages:   make(chan StreamMessage),
 		done:       make(chan struct{}),
-		group:      new(sync.WaitGroup),
 		reader:     reader,
 		httpClient: httpClient,
 	}
+}
+
+func (s *stream) SetUnmarshalHook(hook UnmarshalHook) {
+	s.unmarshalHook = hook
 }
 
 // GetMessages returns the read-only messages channel
@@ -49,7 +58,7 @@ func (s *stream) StopStream() {
 	close(s.done)
 }
 
-// StartStream makes an HTTP GET request to twitter and starts streaming tweets to the Messages channel using Server Sent Events.
+// StartStream makes an HTTP GET request to twitter and starts streaming tweets to the Messages channel.
 // Accepts query params described in GET /2/tweets/search/stream to expand the payload that is returned. Query params string must begin with a ?.
 // See available query params here https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/api-reference/get-tweets-search-stream.
 // See an example here: https://developer.twitter.com/en/docs/twitter-api/expansions.
@@ -61,7 +70,6 @@ func (s *stream) StartStream(optionalQueryParams string) error {
 	}
 
 	s.reader.setStreamResponseBody(res.Body)
-	s.group.Add(1)
 
 	go s.streamMessages(res)
 
@@ -70,10 +78,9 @@ func (s *stream) StartStream(optionalQueryParams string) error {
 
 func (s *stream) streamMessages(res *http.Response) {
 	defer res.Body.Close()
-	defer s.group.Done()
 
 	for !stopped(s.done) {
-		data, err := s.reader.readNext()
+		b, err := s.reader.readNext()
 		if err != nil {
 			s.messages <- StreamMessage{
 				Data: nil,
@@ -82,10 +89,12 @@ func (s *stream) streamMessages(res *http.Response) {
 			s.StopStream()
 			break
 		}
-		if len(data) == 0 {
+		if len(b) == 0 {
 			// empty keep-alive
 			continue
 		}
+
+		data := s.unmarshalHook(b)
 
 		s.messages <- StreamMessage{
 			Data: data,
